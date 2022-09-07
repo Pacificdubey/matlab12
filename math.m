8b4ec1308467d322ae549856ab4c91fc23cfcd56
Prashant dubey <pacific.prashantdubey@gmail.com>
2022-09-07 22:00:54 +0530

    Add files via upload

---
+++ math.m
@@ -0,0 +1,131 @@
+%%
+x = 10
+y = 3
+
+%a
+U = x + y
+
+%b
+V = x*y
+
+%c
+W = x/y
+
+%d
+Z = sin(x)
+
+%e
+R = 8*sin(y);
+
+%f
+S = 5*sin(2*y)
+format default
+%% Part 2 
+x = 2
+y = 5
+
+%a
+(y*x^3)/(y-x)
+
+%b
+(3*x)/(2*y)
+
+%c
+3/(2*y*x)
+
+%d
+(x^5)/(x^5-1)
+
+
+
+%% Part 3
+x = 3
+y = 4
+
+%a
+(1-(1/x^5))^-1
+
+
+%b
+3*pi*(x^2)
+
+%c
+(3*y)/(4*x-8)
+
+
+%d
+(4*(y-5))/(3*x-6)
+
+
+
+%% Part 4
+help rcosine
+
+help cos
+
+help :
+
+help *
+
+help plot
+
+help label
+
+
+%% Part 5
+
+help sqrt
+
+
+%% Part 6
+
+format long
+pi
+
+format short
+pi
+
+format bank
+pi
+
+multiPi = pi*1000
+
+format shortE
+multiPi
+
+%% Part 7
+
+disp(multiPi)
+
+disp("Hello World")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
